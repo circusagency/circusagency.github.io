3f9673be7453760f49073087116c1005f500d582
circusagency <circus.agency2020@gmail.com>
2020-04-13 03:31:33 +0200

    animation1

---
+++ js/fadein.js
@@ -0,0 +1,3 @@
+$(document).ready(function () {
+  $('.testoDIV').delay(1000).animate({"margin-top":"120px", "opacity":"1"}, 1000, "swing");
+});
---
+++ js/fadeinMenu.js
@@ -0,0 +1,3 @@
+$(document).ready(function () {
+  $('#navROW h1').delay(1200).animate({"margin-top":"0px", "opacity":"1"}, 800, "swing");
+});
